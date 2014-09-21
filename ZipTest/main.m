ff4e41b48a5b0f564abbc4cf244a4dda738ece35
Franz Ayestaran <franz@ayestaran.co.uk>
2014-09-21 10:13:15 +0100

    Update main.m

--- ZipTest/main.m
+++ ZipTest/main.m
@@ -1,10 +1,15 @@
-//
-//  main.m
-//  ZipTest
-//
-//  Created by Brandon Trebitowski on 8/6/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+ /*
+ Main.m
+ ZipTest
+ 
+ Developed by Franz Ayestaran on 15/8/14.
+ Copyright (c) 2014 Zonk Technology. All rights reserved.
+ 
+ You may use this code in your own projects and upon doing so, you the programmer are solely
+ responsible for determining it's worthiness for any given application or task. Here clearly
+ states that the code is for learning purposes only and is not guaranteed to conform to any
+ programming style, standard, or be an adequate answer for any given problem.
+ */
 
 #import <UIKit/UIKit.h>
 
